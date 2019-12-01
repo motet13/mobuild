2e26a2ef22116bcec71e5bb0405e023dfe5e81bf
Moses Martinez <mo13gwapo@gmail.com>
2019-12-01 10:29:17 -0500

    improvised vimconfig.sh

--- vimconf/vimconfig.sh
+++ vimconf/vimconfig.sh
@@ -5,34 +5,52 @@ echo -e "$dflt ------------------------- Build my Vim -------------------------"
 echo -e " If Vim is installed but not configured for using Vundle, fix it!"
 echo
 
+# Change this value to ~/.vim
+test_vim_dir=$HOME/gitz/mobuild/vimconf
+
+grn="\e[32m"
+dflt="\e[39m"
+red="\e[91m"
+gry="\e[90m"
+
 # make directories in .vim containing bundle, colors, templates
 # check if vim is installed
-myvim="$(cat logs/result.log | grep vim | awk '{print $2}')"
+myvim="$(cat ../logs/result.log | grep vim | awk '{print $2}')"
 
-# make directories in .vim
-dir_list=$(jq -r '.vim.mkdir[]' config.json)
+# make directories in ~/.vim
+dir_list=$(jq -r '.vim.mkdir[]' ../config.json)
 
 if [ $myvim == '+' ]; then
     for i in $dir_list; do
-        echo -e "$grn [ mkdir ] $gry $HOME/$i"
-#        mkdir -p -v $i
+        echo -en "$grn [ mkdir ] $dflt $test_vim_dir/$i..."
+# This works just uncomment it when ready for production
+#        mkdir -p $test_vim_dir/$i 2> error.log
+        if [[ $? == 0 ]]; then
+            echo -e "$grn Okay $dflt"
+        fi
     done
 fi
 
 # download colorschemes
-colorscheme_list=$(jq -r '.vim.colorscheme[]' config.json)
+colorscheme_list=$(jq -r '.vim.colorscheme[]' ../config.json)
 
 for i in $colorscheme_list; do
     file=$(echo $i | sed 's/\// /g' | awk '{print $NF}')
-    echo -e "$grn [ Downloading ] $gry $i"
-#    curl -o ~/.vim/colors/$file $i
+    echo -en "$grn [ Downloading ] $dflt $file..."
+# This works just uncomment it when ready for production
+    curl -o $test_vim_dir/colors/$file $i >> error.log 2>&1
+    if [[ $? == 0 ]]; then
+        echo -e "$grn Okay $dflt"
+    else
+        echo -e "$red Error $dflt:Please see error.log!"
+    fi
 done
 
 # Create templates (templates will be applied when starting particular file in
 # vim). This will just simply copy the skeleton files from templates directory
 # to your ~/.vim/templates directory. You can edit skeleton files to your
 # likings.
-skeleton_list=$(jq -r '.vim.skeleton[]' config.json)
+skeleton_list=$(jq -r '.vim.skeleton[]' ../config.json)
 
 for i in $skeleton_list; do
     echo -e "$grn [ Copying ] $gry $i"
